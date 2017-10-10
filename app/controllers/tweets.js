947775adfa4cc780d28fca773649261f80db4df8
Paddym1991 <paddym1991.pm@gmail.com>
2017-10-10 18:14:44 +0100

    First handlebar expressions (<title> element (main, login, signup)).

--- app/controllers/tweets.js
+++ app/controllers/tweets.js
@@ -3,21 +3,21 @@
 exports.home = {
 
   handler: (request, reply) => {
-    reply.file('./app/views/main.html');
+    reply.view('main', { title: 'Welcome to Tweeterville' });   //refactor controller to pass actual title to the view when rendered
   },
 };
 
 exports.signup = {
 
   handler: (request, reply) => {
-    reply.file('./app/views/signup.html');
+    reply.view('signup', { title: 'Sign up for MyTweet' });   //refactor controller to pass actual title to the view when rendered
   },
 };
 
 exports.login = {
 
   handler: (request, reply) => {
-    reply.file('./app/views/login.html');
+    reply.view('login', { title: 'Login to MyTweet' });   //refactor controller to pass actual title to the view when rendered
   },
 };
 
