4a47ebbb4a863979342426031a9cae2d59743f47
Paddym1991 <paddym1991.pm@gmail.com>
2017-10-20 21:35:33 +0100

    Installed mongoose-seeder into app. Also preloaded test data using seeder utility.

--- app/models/db.js
+++ app/models/db.js
@@ -13,8 +13,23 @@ if (process.env.NODE_ENV === 'production') {
 
 mongoose.connect(dbURI);
 
+/**
+ * Pre-load database on startup from data.json
+ */
 mongoose.connection.on('connected', function () {
   console.log('Mongoose connected to ' + dbURI);
+  if (process.env.NODE_ENV != 'production') {
+    var seeder = require('mongoose-seeder');
+    const data = require('./data.json');
+    const Tweet = require('./tweet');
+    const User = require('./user');
+    seeder.seed(data, { dropDatabase: false, dropCollections: true }).then(dbData => {
+      console.log('preloading Test Data');
+      console.log(dbData);
+    }).catch(err => {
+      console.log(error);
+    });
+  }
 });
 
 mongoose.connection.on('error', function (err) {
