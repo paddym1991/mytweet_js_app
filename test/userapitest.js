1795b8cee0d9ea1ea8d649576b68b18f7f68a62f
Paddym1991 <paddym1991.pm@gmail.com>
2017-11-05 19:35:09 +0000

    Api: First Mocha API test.

--- test/userapitest.js
+++ test/userapitest.js
@@ -1,15 +1,53 @@
 'use strict';
 
 const assert = require('chai').assert;
+let request = require('sync-request');
 
 /**
- * This is a simple test, guaranteed to succeed
+ * Revised version of first test to include sync-request
  */
 suite('User API tests', function () {
 
   test('get users', function () {
 
-    assert.equal(1, 1);   //try 'assert.equal(1, 2)' to guarantee a failed test
+    const url = 'http://localhost:4000/api/users';
+    const res = request('GET', url);
+    const users = JSON.parse(res.getBody('utf8'));
+
+    assert.equal(4, users.length);
+
+    assert.equal(users[0].firstName, 'Homer');
+    assert.equal(users[0].lastName, 'Simpson');
+    assert.equal(users[0].email, 'homer@simpson.com');
+
+    assert.equal(users[1].firstName, 'Marge');
+    assert.equal(users[1].lastName, 'Simpson');
+    assert.equal(users[1].email, 'marge@simpson.com');
+
+    assert.equal(users[2].firstName, 'Bart');
+    assert.equal(users[2].lastName, 'Simpson');
+    assert.equal(users[2].email, 'bart@simpson.com');
+
+    assert.equal(users[3].firstName, 'Admin');
+    assert.equal(users[3].lastName, 'Istrator');
+    assert.equal(users[3].email, 'admin@istrator.com');
 
   });
+
+  test('get one user', function () {
+
+    const allUsersUrl = 'http://localhost:4000/api/users';
+    let res = request('GET', allUsersUrl);
+    const users = JSON.parse(res.getBody('utf8'));
+
+    const oneUserUrl = allUsersUrl + '/' + users[0]._id;
+    res = request('GET', oneUserUrl);
+    const oneUser = JSON.parse(res.getBody('utf8'));
+
+    assert.equal(oneUser.firstName, 'Homer');
+    assert.equal(oneUser.lastName, 'Simpson');
+    assert.equal(oneUser.email, 'homer@simpson.com');
+
+  });
+
 });
