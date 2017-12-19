d4eafcd7aebac85c5880cf83a8ce2c715f871336
Paddym1991 <paddym1991.pm@gmail.com>
2017-12-19 15:02:23 +0000

    Authentication route.

--- app/api/usersapi.js
+++ app/api/usersapi.js
@@ -2,6 +2,7 @@
 
 const User = require('../models/user');
 const Boom = require('boom');
+const utils = require('/utils.js');
 
 //api to find all users
 exports.find = {
@@ -87,6 +88,24 @@ exports.deleteOne = {
       reply(user).code(204);
     }).catch(err => {
       reply(Boom.notFound('id not found'));
+    });
+  },
+
+};
+
+exports.authenticate = {
+  auth: false,
+  handler: function (request, reply) {
+    const user = request.payload;
+    User.findOne({ email: user.email }).then(foundUser => {
+      if (foundUser && foundUser.password === user.password) {
+        const token = utils.createToken(foundUser);
+        reply({ success: true, token: token }).code(201);
+      } else {
+        reply({ success: false, message: 'Authentication failed. User not found.' }).code(201);
+      }
+    }).catch(err => {
+      reply(Boom.notFound('internal db failure'));
     });
   },
 
